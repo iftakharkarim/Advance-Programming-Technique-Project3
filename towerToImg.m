094fddaa1bde85efd6e50e9c163d2a2311061b94
Nathan Olivares <nkotechno@gmail.com>
2018-12-08 16:47:57 -0500

    Create towerToImg.m
    Run this to see what the 2d dft should be doing with shifted values, I think. Everything is a mirror of the first quadrant

---
+++ towerToImg.m
@@ -0,0 +1,24 @@
+close all
+file = fopen('Tower256.txt');
+
+a = fscanf(file,'%i');
+dim = a(1);
+a = a(3:end);
+b = zeros(dim,dim);
+
+for i = 1:(dim*dim)
+   b(i) = a(i); 
+    
+end
+
+b = b;
+
+I = mat2gray(b);
+imshow(I)
+figure
+b2 = fft2(b);
+imshow(log(abs(b2)),[])
+% highest freq above is in the middle
+
+imshow(fftshift(log(abs(b2))),[])
+% now the lowest frequency is in the middle, highest are at the edges
