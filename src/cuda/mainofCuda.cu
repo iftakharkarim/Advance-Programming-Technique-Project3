fa2745da11b7a42955f3a23627b51382b7bad47d
Nathan Olivares <nkotechno@gmail.com>
2018-12-11 16:56:04 -0500

    Fixed a few bugs
    Made the complex library work on the gpu (I think) and a few other issues in the main body. Can't currently compile on pace so I can't do much more until paces is fixed

--- src/cuda/mainofCuda.cu
+++ src/cuda/mainofCuda.cu
@@ -9,8 +9,11 @@ using namespace std;
 
 #define M_PI 3.14159265358979323846
 
+// easy copy paste qsub
+// qsub -I -q coc-ice -l nodes=1:ppn=1:nvidiagpu:teslap100,walltime=2:00:00,pmem=2gb
+
 // device function which does row wise DFT
-__global__ doDFT1(Complex *gpuArray1, Complex *gpuArray2, int width, int height, float M_PI) {
+__global__ void doDFT1(Complex *gpuArray1, Complex *gpuArray2, int width, int height, float M_PI) {
 	int index = threadIdx.x + blockIdx.x * blockDim.x;
 	index = min(index, width*height - 1);
 	int y = index / width; // current row
@@ -22,7 +25,7 @@ __global__ doDFT1(Complex *gpuArray1, Complex *gpuArray2, int width, int height,
 	gpuArray1[index] = value;
 }
 // device function which does column wise DFT
-__global__ doDFT2(Complex *gpuArray1, Complex *gpuArray2, int width, int height, float M_PI) {
+__global__ void doDFT2(Complex *gpuArray1, Complex *gpuArray2, int width, int height, float M_PI) {
 	int index = threadIdx.x + blockIdx.x * blockDim.x;
 	index = min(index, width*height - 1);
 	int x = index % width; // current column
@@ -45,6 +48,7 @@ int main(int argc, char const **argv)
 	}
 	
 	InputImage input = InputImage(argv[2]);
+	InputImage output = InputImage(argv[3]);
 	int width = input.get_width();
 	int height = input.get_height();
 	cout<<"w  "<<width<<"  h  "<<height<<endl;
@@ -52,14 +56,17 @@ int main(int argc, char const **argv)
 
 	cout<<input.get_image_data()<<endl;
 	Complex *inputArray = input.get_image_data();
-	cout<<*inputArray<<endl;
+	cout<<*inputArray<<endl; // reading from pointer
 	cout<<*(inputArray+1)<<endl;
 
 	// define, allocate and copy data for gpu
-	Complex *gpuArray1, gpuArray2;
-	int size = w*h*sizeof(Complex);
-	cudaMalloc(&gpuArray1, size);
-	cudaMalloc(&gpuArray2, size);
+	Complex *gpuArray1, *gpuArray2;
+	int size = width*height*sizeof(Complex);
+
+	cudaMalloc((void**)&gpuArray1, size);
+	cudaMalloc((void**)&gpuArray2, size);
+	//cudaMalloc(&gpuArray1, size);
+	//cudaMalloc(&gpuArray2, size);
 
 	cudaMemcpy(gpuArray1, &inputArray, size, cudaMemcpyHostToDevice);
 	cudaMemcpy(gpuArray2, &inputArray, size, cudaMemcpyHostToDevice);
@@ -82,6 +89,6 @@ int main(int argc, char const **argv)
 	cudaFree(gpuArray1), cudaFree(gpuArray2);
 
 	// write output into the given text file
-	input.save_image_data(argv[2], input, width, height);
+	input.save_image_data(argv[3], inputArray, width, height);
 	return 0;
 }
